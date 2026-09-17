633e8dccaaceadc255e0dd8f5abe2d2eac330df4
Kevin Dizon <kevindizon@Kevins-MacBook-Pro.local>
2026-09-17 23:41:56 +0800

    Update API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://love-book-backend.onrender.com/api'
+  apiUrl: 'https://love-book-backend-1.onrender.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://love-book-backend.onrender.com/api'
+  apiUrl: 'https://love-book-backend-1.onrender.com/api'
 };
